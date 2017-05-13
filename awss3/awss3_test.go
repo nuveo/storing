f3d08aeeef6ea2ad3ae8152cea3b7510324b9bb0
Cesar Gimenes <crg@crg.eti.br>
2017-05-13 11:48:21 -0300

    add tests

---
+++ awss3/awss3_test.go
@@ -0,0 +1,44 @@
+package awss3
+
+import (
+	"bytes"
+	"io/ioutil"
+	"testing"
+)
+
+func TestS3(t *testing.T) {
+
+	bUp, err := ioutil.ReadFile("../testdata/testfile.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Upload
+	_, err = Upload("testfile.txt", "text/plain", bUp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Download
+	var bDown []byte
+	bDown, err = Download("testfile.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if bytes.Compare(bUp, bDown) != 0 {
+		t.Fatal("Uploaded data are different from the data received in the download.")
+	}
+
+	// Delete
+	err = Delete("testfile.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = Download("testfile.txt")
+	if err == nil {
+		t.Fatal("An error was expected")
+	}
+
+}
