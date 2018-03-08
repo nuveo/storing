b1d57359eead3974a0b617729909f4e65a91a62d
Cássio Botaro <cassiobotaro@gmail.com>
2018-03-08 14:36:19 -0300

    Shadowing error (#8)
    * declare new error to avoid shadowing error
    
    * log correct error

--- awss3/awss3.go
+++ awss3/awss3.go
@@ -93,9 +93,9 @@ func (s *Storing) Download(path string) (b []byte, err error) {
 		return
 	}
 	defer func() {
-		err = os.Remove(tmpfile.Name())
-		if err != nil {
-			log.Errorln(err)
+		rmErr := os.Remove(tmpfile.Name())
+		if rmErr != nil {
+			log.Errorln(rmErr)
 		}
 	}()
 
