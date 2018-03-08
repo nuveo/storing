62cf9a5b6501dd8b9af380e4819778256698b1c2
Felipe Oliveira <felipeweb.programador@gmail.com>
2018-03-08 12:05:28 -0300

    refactoring storing to make more customizable (#7)
    * refactoring storing to make more customizable
    
    * fatal in test

--- awss3/awss3.go
+++ awss3/awss3.go
@@ -9,11 +9,52 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/nuveo/log"
 )
 
 // Storing implementation for aws s3
 type Storing struct {
-	session *session.Session
+	Session *session.Session
+	Bucket  string
+	ACL     string
+}
+
+// Options for AWS s3 Storing
+type Options struct {
+	Session *session.Session
+	Bucket  string
+	ACL     string
+}
+
+func startS3Session() (s *session.Session, err error) {
+	s, err = session.NewSession()
+	return
+}
+
+// New s3 storing
+func New(opts ...Options) (s *Storing, err error) {
+	opt := Options{}
+	if len(opts) > 0 {
+		opt = opts[0]
+	}
+	if opt.Session == nil {
+		opt.Session, err = startS3Session()
+		if err != nil {
+			return
+		}
+	}
+	if opt.Bucket == "" {
+		opt.Bucket = os.Getenv("AWS_BUCKET")
+	}
+	if opt.ACL == "" {
+		opt.ACL = os.Getenv("AWS_ACL")
+	}
+	s = &Storing{
+		Session: opt.Session,
+		Bucket:  opt.Bucket,
+		ACL:     opt.ACL,
+	}
+	return
 }
 
 // Provider returns the name of the provider of the current adapter.
@@ -21,27 +62,14 @@ func (s *Storing) Provider() string {
 	return "s3"
 }
 
-func (s *Storing) startS3Session() (err error) {
-	if s.session == nil {
-		s.session, err = session.NewSession()
-		return
-	}
-	return
-}
-
 // Upload upload file to S3
 func (s *Storing) Upload(name string, contentType string, content []byte) (path string, err error) {
-	err = s.startS3Session()
-	if err != nil {
-		return
-	}
-
-	uploader := s3manager.NewUploader(s.session)
+	uploader := s3manager.NewUploader(s.Session)
 
 	var file *s3manager.UploadOutput
 	file, err = uploader.Upload(&s3manager.UploadInput{
-		Bucket:      aws.String(os.Getenv("AWS_BUCKET")),
-		ACL:         aws.String(os.Getenv("AWS_ACL")),
+		Bucket:      aws.String(s.Bucket),
+		ACL:         aws.String(s.ACL),
 		Key:         aws.String(name),
 		ContentType: aws.String(contentType),
 		Body:        bytes.NewReader(content),
@@ -55,12 +83,7 @@ func (s *Storing) Upload(name string, contentType string, content []byte) (path 
 
 // Download file from s3
 func (s *Storing) Download(path string) (b []byte, err error) {
-	err = s.startS3Session()
-	if err != nil {
-		return
-	}
-
-	downloader := s3manager.NewDownloader(s.session)
+	downloader := s3manager.NewDownloader(s.Session)
 	if err != nil {
 		return
 	}
@@ -69,10 +92,15 @@ func (s *Storing) Download(path string) (b []byte, err error) {
 	if err != nil {
 		return
 	}
-	defer os.Remove(tmpfile.Name())
+	defer func() {
+		err = os.Remove(tmpfile.Name())
+		if err != nil {
+			log.Errorln(err)
+		}
+	}()
 
 	_, err = downloader.Download(tmpfile, &s3.GetObjectInput{
-		Bucket: aws.String("nuveofs"),
+		Bucket: aws.String(s.Bucket),
 		Key:    aws.String(path),
 	})
 	if err != nil {
@@ -85,14 +113,9 @@ func (s *Storing) Download(path string) (b []byte, err error) {
 
 // Delete from s3
 func (s *Storing) Delete(key string) (err error) {
-	err = s.startS3Session()
-	if err != nil {
-		return
-	}
-
-	svc := s3.New(s.session)
+	svc := s3.New(s.Session)
 	obj := &s3.DeleteObjectInput{
-		Bucket: aws.String("nuveofs"),
+		Bucket: aws.String(s.Bucket),
 		Key:    aws.String(key),
 	}
 	_, err = svc.DeleteObject(obj)
